ccaff2efd15ba844685c4975d35965352b7f8f6a
imiguel <axtcas900>
2015-11-12 12:29:24 +0000

    Mais o toast

--- app/src/main/java/pt/ipbeja/estig/planetslist/MainActivity.java
+++ app/src/main/java/pt/ipbeja/estig/planetslist/MainActivity.java
@@ -2,15 +2,18 @@ package pt.ipbeja.estig.planetslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
     private List<Map<String, Object>> planets;
     private String [] from;
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lstPlanets = (ListView) findViewById(R.id.lstPlanets);
         lstPlanets.setAdapter(sa);
+        lstPlanets.setOnItemClickListener(this);
     }
 
     private List<Map<String, Object>> getPlanetsData(){
@@ -63,5 +67,11 @@ public class MainActivity extends AppCompatActivity {
         item.put(this.from[2], planetSize);
 
         return item;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String info = "Pos = " + position + " id = " + id;
+        Toast.makeText(this, info, Toast.LENGTH_SHORT).show();
     }
 }
